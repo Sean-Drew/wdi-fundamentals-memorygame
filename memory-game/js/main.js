a7b9b3a896695c410fe8a32a4925ffce2556d0e9
Sean Drew <SDrew989@gmail.com>
2020-03-07 20:16:39 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,17 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "king";
-let cardFour = "queen";
-console.log("User flipped" + cardOne);
-console.log("User flipped" + cardThree);
+let cards = ["queen", "queen", "king", "king"];
+
+let cardsInPlay = [];
+
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped" + " " + cardsInPlay[0]);
+console.log("User flipped" + " " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}

b0178e71f121a0ece8d21e2cb4b7f21af6f218e4
Sean Drew <SDrew989@gmail.com>
2020-03-08 14:55:57 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,16 +2,19 @@ let cards = ["queen", "queen", "king", "king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
-let cardTwo = cards[2];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped" + " " + cardsInPlay[0]);
-console.log("User flipped" + " " + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  		console.log("You found a match!");
+	} else {
+  		console.log("Sorry, try again.");
+	}
 }
+
+function flipCard(cardID) {
+	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped" + " " + cards[cardID]);
+	checkForMatch();
+}
+
+flipCard(0); // calling flipCard function
+flipCard(2); // calling flipCard function
